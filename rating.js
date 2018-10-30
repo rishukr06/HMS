adee03496a77b664af21c08a7fc1a25822cfcdf8
Rishu kumar <38401347+Rishu-kumar@users.noreply.github.com>
2018-10-30 22:51:10 +0530

    Add files via upload

---
+++ rating.js
@@ -0,0 +1,215 @@
+function check1() {
+			//body...
+			document.getElementById('checked1').innerHTML = '&#x2605';
+			document.getElementById('checked2').innerHTML = '&#x2606';
+			document.getElementById('checked3').innerHTML = '&#x2606';
+			document.getElementById('checked4').innerHTML = '&#x2606';
+			document.getElementById('checked5').innerHTML = '&#x2606';
+			//rating color
+			document.getElementById('checked1').style.color="red";
+			document.getElementById('checked2').style.color="red";
+			document.getElementById('checked3').style.color="red";
+			document.getElementById('checked4').style.color="red";
+			document.getElementById('checked5').style.color="red";
+		}
+		function check2() {
+			// body...
+			document.getElementById('checked1').innerHTML = '&#x2605';
+			document.getElementById('checked2').innerHTML = '&#x2605';
+			document.getElementById('checked3').innerHTML = '&#x2606';
+			document.getElementById('checked4').innerHTML = '&#x2606';
+			document.getElementById('checked5').innerHTML = '&#x2606';
+			//rating color
+			document.getElementById('checked1').style.color="red";
+			document.getElementById('checked2').style.color="red";
+			document.getElementById('checked3').style.color="red";
+			document.getElementById('checked4').style.color="red";
+			document.getElementById('checked5').style.color="red";
+		}
+		function check3() {
+			// body...
+			document.getElementById('checked1').innerHTML = '&#x2605';
+			document.getElementById('checked2').innerHTML = '&#x2605';
+			document.getElementById('checked3').innerHTML = '&#x2605';
+			document.getElementById('checked4').innerHTML = '&#x2606';
+			document.getElementById('checked5').innerHTML = '&#x2606';
+			//rating color
+			document.getElementById('checked1').style.color="yellow";
+			document.getElementById('checked2').style.color="yellow";
+			document.getElementById('checked3').style.color="yellow";
+			document.getElementById('checked4').style.color="yellow";
+			document.getElementById('checked5').style.color="yellow";
+		}
+		function check4() {
+			// body...
+			document.getElementById('checked1').innerHTML = '&#x2605';
+			document.getElementById('checked2').innerHTML = '&#x2605';
+			document.getElementById('checked3').innerHTML = '&#x2605';
+			document.getElementById('checked4').innerHTML = '&#x2605';
+			document.getElementById('checked5').innerHTML = '&#x2606';
+			//rating color
+			document.getElementById('checked1').style.color="yellow";
+			document.getElementById('checked2').style.color="yellow";
+			document.getElementById('checked3').style.color="yellow";
+			document.getElementById('checked4').style.color="yellow";
+			document.getElementById('checked5').style.color="yellow";
+		}
+		function check5() {
+			// body...
+			document.getElementById('checked1').innerHTML = '&#x2605';
+			document.getElementById('checked2').innerHTML = '&#x2605';
+			document.getElementById('checked3').innerHTML = '&#x2605';
+			document.getElementById('checked4').innerHTML = '&#x2605';
+			document.getElementById('checked5').innerHTML = '&#x2605';
+			//rating color
+			document.getElementById('checked1').style.color="yellow";
+			document.getElementById('checked2').style.color="yellow";
+			document.getElementById('checked3').style.color="yellow";
+			document.getElementById('checked4').style.color="yellow";
+			document.getElementById('checked5').style.color="yellow";
+		}
+
+
+// second room
+function check11() {
+			//body...
+			document.getElementById('checked11').innerHTML = '&#x2605';
+			document.getElementById('checked21').innerHTML = '&#x2606';
+			document.getElementById('checked31').innerHTML = '&#x2606';
+			document.getElementById('checked41').innerHTML = '&#x2606';
+			document.getElementById('checked51').innerHTML = '&#x2606';
+			//rating color
+			document.getElementById('checked11').style.color="red";
+			document.getElementById('checked21').style.color="red";
+			document.getElementById('checked31').style.color="red";
+			document.getElementById('checked41').style.color="red";
+			document.getElementById('checked51').style.color="red";
+		}
+		function check21() {
+			// body...
+			document.getElementById('checked11').innerHTML = '&#x2605';
+			document.getElementById('checked21').innerHTML = '&#x2605';
+			document.getElementById('checked31').innerHTML = '&#x2606';
+			document.getElementById('checked41').innerHTML = '&#x2606';
+			document.getElementById('checked51').innerHTML = '&#x2606';
+			//rating color
+			document.getElementById('checked11').style.color="red";
+			document.getElementById('checked21').style.color="red";
+			document.getElementById('checked31').style.color="red";
+			document.getElementById('checked41').style.color="red";
+			document.getElementById('checked51').style.color="red";
+		}
+		function check31() {
+			// body...
+			document.getElementById('checked11').innerHTML = '&#x2605';
+			document.getElementById('checked21').innerHTML = '&#x2605';
+			document.getElementById('checked31').innerHTML = '&#x2605';
+			document.getElementById('checked41').innerHTML = '&#x2606';
+			document.getElementById('checked51').innerHTML = '&#x2606';
+			//rating color
+			document.getElementById('checked11').style.color="yellow";
+			document.getElementById('checked21').style.color="yellow";
+			document.getElementById('checked31').style.color="yellow";
+			document.getElementById('checked41').style.color="yellow";
+			document.getElementById('checked51').style.color="yellow";
+		}
+		function check41() {
+			// body...
+			document.getElementById('checked11').innerHTML = '&#x2605';
+			document.getElementById('checked21').innerHTML = '&#x2605';
+			document.getElementById('checked31').innerHTML = '&#x2605';
+			document.getElementById('checked41').innerHTML = '&#x2605';
+			document.getElementById('checked51').innerHTML = '&#x2606';
+			//rating color
+			document.getElementById('checked11').style.color="yellow";
+			document.getElementById('checked21').style.color="yellow";
+			document.getElementById('checked31').style.color="yellow";
+			document.getElementById('checked41').style.color="yellow";
+			document.getElementById('checked51').style.color="yellow";
+		}
+		function check51() {
+			// body...
+			document.getElementById('checked11').innerHTML = '&#x2605';
+			document.getElementById('checked21').innerHTML = '&#x2605';
+			document.getElementById('checked31').innerHTML = '&#x2605';
+			document.getElementById('checked41').innerHTML = '&#x2605';
+			document.getElementById('checked51').innerHTML = '&#x2605';
+			//rating color
+			document.getElementById('checked11').style.color="yellow";
+			document.getElementById('checked21').style.color="yellow";
+			document.getElementById('checked31').style.color="yellow";
+			document.getElementById('checked41').style.color="yellow";
+			document.getElementById('checked51').style.color="yellow";
+		}
+
+// 3rd room rating
+function check12() {
+			//body...
+			document.getElementById('checked12').innerHTML = '&#x2605';
+			document.getElementById('checked22').innerHTML = '&#x2606';
+			document.getElementById('checked32').innerHTML = '&#x2606';
+			document.getElementById('checked42').innerHTML = '&#x2606';
+			document.getElementById('checked52').innerHTML = '&#x2606';
+			//rating color
+			document.getElementById('checked12').style.color="red";
+			document.getElementById('checked22').style.color="red";
+			document.getElementById('checked32').style.color="red";
+			document.getElementById('checked42').style.color="red";
+			document.getElementById('checked52').style.color="red";
+		}
+		function check22() {
+			// body...
+			document.getElementById('checked12').innerHTML = '&#x2605';
+			document.getElementById('checked22').innerHTML = '&#x2605';
+			document.getElementById('checked32').innerHTML = '&#x2606';
+			document.getElementById('checked42').innerHTML = '&#x2606';
+			document.getElementById('checked52').innerHTML = '&#x2606';
+			//rating color
+			document.getElementById('checked12').style.color="red";
+			document.getElementById('checked22').style.color="red";
+			document.getElementById('checked32').style.color="red";
+			document.getElementById('checked42').style.color="red";
+			document.getElementById('checked52').style.color="red";
+		}
+		function check32() {
+			// body...
+			document.getElementById('checked12').innerHTML = '&#x2605';
+			document.getElementById('checked22').innerHTML = '&#x2605';
+			document.getElementById('checked32').innerHTML = '&#x2605';
+			document.getElementById('checked42').innerHTML = '&#x2606';
+			document.getElementById('checked52').innerHTML = '&#x2606';
+			//rating color
+			document.getElementById('checked12').style.color="yellow";
+			document.getElementById('checked22').style.color="yellow";
+			document.getElementById('checked32').style.color="yellow";
+			document.getElementById('checked42').style.color="yellow";
+			document.getElementById('checked52').style.color="yellow";
+		}
+		function check42() {
+			// body...
+			document.getElementById('checked12').innerHTML = '&#x2605';
+			document.getElementById('checked22').innerHTML = '&#x2605';
+			document.getElementById('checked32').innerHTML = '&#x2605';
+			document.getElementById('checked42').innerHTML = '&#x2605';
+			document.getElementById('checked52').innerHTML = '&#x2606';
+			//rating color
+			document.getElementById('checked12').style.color="yellow";
+			document.getElementById('checked22').style.color="yellow";
+			document.getElementById('checked32').style.color="yellow";
+			document.getElementById('checked42').style.color="yellow";
+			document.getElementById('checked52').style.color="yellow";
+		}
+		function check52() {
+			// body...
+			document.getElementById('checked12').innerHTML = '&#x2605';
+			document.getElementById('checked22').innerHTML = '&#x2605';
+			document.getElementById('checked32').innerHTML = '&#x2605';
+			document.getElementById('checked42').innerHTML = '&#x2605';
+			document.getElementById('checked52').innerHTML = '&#x2605';
+			//rating color
+			document.getElementById('checked12').style.color="yellow";
+			document.getElementById('checked22').style.color="yellow";
+			document.getElementById('checked32').style.color="yellow";
+			document.getElementById('checked42').style.color="yellow";
+			document.getElementById('checked52').style.color="yellow";
+		}
